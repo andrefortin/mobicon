e20eab5bca029191f66e061b79421991aa033ecb
Andre Fortin <andre@leadcarrot.io>
2021-02-09 19:09:00 -0500

    Update test framework to Jest, added Chrome extension platform

--- index.js
+++ index.js
@@ -4,37 +4,40 @@ const pathExists = require('path-exists');
 const pify = require('pify');
 const gm = require('gm');
 const mkdir = require('mkdirp');
-const pwaIcons = require('pwa-icon-list');
-const androidIcons = require('android-icon-list');
-const bb10Icons = require('bb10-icon-list');
-const iosIcons = require('ios-icon-list');
 const execa = require('execa');
 const mask = require('./lib/mask');
+const androidIcons = require('android-icon-list');
+const bb10Icons = require('bb10-icon-list');
+const chromeIcons = require('chrome-icon-list');
+const iosIcons = require('ios-icon-list');
+const pwaIcons = require('pwa-icon-list');
 
 const mkdirp = pify(mkdir);
 
 const platformIcons = {
-	pwa: pwaIcons(),
 	android: androidIcons(),
+	blackberry10: bb10Icons(),
+	chrome: chromeIcons(),
 	ios: iosIcons(),
-	blackberry10: bb10Icons()
+	pwa: pwaIcons()
 };
 
 // See https://material.io/design/platform-guidance/android-icons.html#keyline-shapes
 const platformRadius = new Map([
 	['android', 0.0909],
+	['chrome', 0.0909],
 	['pwa', 0.0909]
 ]);
 
-const calculateDimension = (imgSize, iconSize, opts, resizeFn) => {
+const calculateDimension = (imgSize, iconSize, options, resizeFn) => {
 	let width;
 	let height;
 
 	if (imgSize.width > imgSize.height) {
-		width = iconSize * opts.contentRatio;
+		width = iconSize * options.contentRatio;
 		height = imgSize.height / imgSize.width * width;
 	} else {
-		height = iconSize * opts.contentRatio;
+		height = iconSize * options.contentRatio;
 		width = imgSize.width / imgSize.height * height;
 	}
 
@@ -47,29 +50,30 @@ const calculateDimension = (imgSize, iconSize, opts, resizeFn) => {
 	return {width, height};
 };
 
-module.exports = (file, opts) => {
+module.exports = (file, options) => {
 	if (typeof file !== 'string' || !pathExists.sync(file)) {
 		return Promise.reject(new TypeError('Expected a file.'));
 	}
 
-	opts = Object.assign({
+	options = Object.assign({
 		platform: '',
 		dest: process.cwd(),
 		background: 'white',
-		roundedCorners: platformRadius.has(opts.platform),
-		borderRadius: platformRadius.get(opts.platform),
+		roundedCorners: platformRadius.has(options.platform),
+		borderRadius: platformRadius.get(options.platform),
 		contentRatio: 1
-	}, opts);
+	}, options);
 
-	if (opts.platform === '') {
+	if (options.platform === '') {
 		return Promise.reject(new Error('Please provide a platform'));
 	}
 
-	if (Object.keys(platformIcons).indexOf(opts.platform.toLowerCase()) === -1) {
-		return Promise.reject(new Error(`Platform ${opts.platform} is not supported.`));
+	const isFound = Object.keys(platformIcons).includes(options.platform.toLowerCase());
+	if (!isFound) {
+		return Promise.reject(new Error(`Platform ${options.platform} is not supported.`));
 	}
 
-	const icons = platformIcons[opts.platform.toLowerCase()];
+	const icons = platformIcons[options.platform.toLowerCase()];
 	const resizeFn = path.extname(file) === '.svg' ? 'density' : 'resize';
 
 	const img = gm(file);
@@ -79,19 +83,19 @@ module.exports = (file, opts) => {
 			const {size} = identity;
 
 			return Promise.all(icons.map(icon => {
-				const dest = path.join(opts.dest, icon.file);
-				const dimension = calculateDimension(size, icon.dimension, opts, resizeFn);
+				const dest = path.join(options.dest, icon.file);
+				const dimension = calculateDimension(size, icon.dimension, options, resizeFn);
 
 				const image = gm(file)[resizeFn](dimension.width, dimension.height)
 					.gravity('Center')
-					.background(opts.background)
+					.background(options.background)
 					.extent(icon.dimension, icon.dimension);
 
 				return mkdirp(path.dirname(dest))
 					.then(() => pify(image.write.bind(image))(dest))
 					.then(() => {
-						if (opts.roundedCorners) {
-							return mask(icon.dimension, opts.borderRadius)
+						if (options.roundedCorners) {
+							return mask(icon.dimension, options.borderRadius)
 								.then(maskLocation => {
 									// Apply the mask and overwrite the original image
 									return execa('gm', ['composite', '-compose', 'in', dest, maskLocation, dest]);
--- test.js
+++ test.js
@@ -1,84 +1,301 @@
-import path from 'path';
-import fs from 'fs';
-import test from 'ava';
-import pathExists from 'path-exists';
-import tempfile from 'tempfile';
-import gm from 'gm';
-import pify from 'pify';
-import parsePNG from 'parse-png';
-import fn from '.';
+const path = require('path');
+const fs = require('fs');
+const pathExists = require('path-exists');
+const tempfile = require('tempfile');
+const gm = require('gm');
+const pify = require('pify');
+const parsePNG = require('parse-png');
+const fn = require('./index');
 
-test.beforeEach(t => {
-	t.context.tmp = tempfile();
+describe('android platform', () => {
+	test('android - png input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.png', { platform: 'android', dest: tempFile });
+
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'mipmap-ldpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-mdpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-hdpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-xhdpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-xxhdpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-xxxhdpi/icon.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
+
+	test('android - svg input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.svg', { platform: 'android', dest: tempFile });
+
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'mipmap-ldpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-mdpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-hdpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-xhdpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-xxhdpi/icon.png')),
+			pathExists(path.join(tempFile, 'mipmap-xxxhdpi/icon.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
 });
 
-test('pwa', async t => {
-	await fn('fixtures/icon.png', {platform: 'pwa', dest: t.context.tmp});
+describe('blackberry10 platform', () => {
+	test('blackberry10 - png input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.png', { platform: 'blackberry10', dest: tempFile });
 
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-72x72.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-96x96.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-128x128.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-144x144.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-152x152.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-192x192.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-384x384.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-512x512.png')));
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'icon-90.png')),
+			pathExists(path.join(tempFile, 'icon-96.png')),
+			pathExists(path.join(tempFile, 'icon-110.png')),
+			pathExists(path.join(tempFile, 'icon-144.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
+
+	test('blackberry10 - svg input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.svg', { platform: 'blackberry10', dest: tempFile });
+
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'icon-90.png')),
+			pathExists(path.join(tempFile, 'icon-96.png')),
+			pathExists(path.join(tempFile, 'icon-110.png')),
+			pathExists(path.join(tempFile, 'icon-144.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
 });
 
-test('android', async t => {
-	await fn('fixtures/icon.png', {platform: 'android', dest: t.context.tmp});
+describe('chrome platform', () => {
+	test('chrome - png input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.png', { platform: 'chrome', dest: tempFile });
 
-	t.true(pathExists.sync(path.join(t.context.tmp, 'mipmap-ldpi/icon.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'mipmap-mdpi/icon.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'mipmap-hdpi/icon.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'mipmap-xhdpi/icon.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'mipmap-xxhdpi/icon.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'mipmap-xxxhdpi/icon.png')));
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'icon-16.png')),
+			pathExists(path.join(tempFile, 'icon-32.png')),
+			pathExists(path.join(tempFile, 'icon-48.png')),
+			pathExists(path.join(tempFile, 'icon-128.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
+
+	test('chrome - svg input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.svg', { platform: 'chrome', dest: tempFile });
+
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'icon-16.png')),
+			pathExists(path.join(tempFile, 'icon-32.png')),
+			pathExists(path.join(tempFile, 'icon-48.png')),
+			pathExists(path.join(tempFile, 'icon-128.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
 });
 
-test('ios', async t => {
-	await fn('fixtures/icon.png', {platform: 'ios', dest: t.context.tmp});
+describe('ios platform', () => {
+	test('ios - png input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.png', { platform: 'ios', dest: tempFile });
 
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon.png')));
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'icon.png')),
+			pathExists(path.join(tempFile, 'icon@2x.png')),
+			pathExists(path.join(tempFile, 'icon-20.png')),
+			pathExists(path.join(tempFile, 'icon-40.png')),
+			pathExists(path.join(tempFile, 'icon-40@2x.png')),
+			pathExists(path.join(tempFile, 'icon-40@3x.png')),
+			pathExists(path.join(tempFile, 'icon-1024.png')),
+			pathExists(path.join(tempFile, 'icon-50.png')),
+			pathExists(path.join(tempFile, 'icon-50@2x.png')),
+			pathExists(path.join(tempFile, 'icon-60.png')),
+			pathExists(path.join(tempFile, 'icon-60@2x.png')),
+			pathExists(path.join(tempFile, 'icon-60@3x.png')),
+			pathExists(path.join(tempFile, 'icon-72.png')),
+			pathExists(path.join(tempFile, 'icon-72@2x.png')),
+			pathExists(path.join(tempFile, 'icon-76.png')),
+			pathExists(path.join(tempFile, 'icon-76@2x.png')),
+			pathExists(path.join(tempFile, 'icon-83.5@2x.png')),
+			pathExists(path.join(tempFile, 'icon-small.png')),
+			pathExists(path.join(tempFile, 'icon-small@2x.png')),
+			pathExists(path.join(tempFile, 'icon-small@3x.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
+
+	test('ios - svg input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.svg', { platform: 'ios', dest: tempFile });
+
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'icon.png')),
+			pathExists(path.join(tempFile, 'icon@2x.png')),
+			pathExists(path.join(tempFile, 'icon-20.png')),
+			pathExists(path.join(tempFile, 'icon-40.png')),
+			pathExists(path.join(tempFile, 'icon-40@2x.png')),
+			pathExists(path.join(tempFile, 'icon-40@3x.png')),
+			pathExists(path.join(tempFile, 'icon-1024.png')),
+			pathExists(path.join(tempFile, 'icon-50.png')),
+			pathExists(path.join(tempFile, 'icon-50@2x.png')),
+			pathExists(path.join(tempFile, 'icon-60.png')),
+			pathExists(path.join(tempFile, 'icon-60@2x.png')),
+			pathExists(path.join(tempFile, 'icon-60@3x.png')),
+			pathExists(path.join(tempFile, 'icon-72.png')),
+			pathExists(path.join(tempFile, 'icon-72@2x.png')),
+			pathExists(path.join(tempFile, 'icon-76.png')),
+			pathExists(path.join(tempFile, 'icon-76@2x.png')),
+			pathExists(path.join(tempFile, 'icon-83.5@2x.png')),
+			pathExists(path.join(tempFile, 'icon-small.png')),
+			pathExists(path.join(tempFile, 'icon-small@2x.png')),
+			pathExists(path.join(tempFile, 'icon-small@3x.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
 });
 
-test('bb10', async t => {
-	await fn('fixtures/icon.png', {platform: 'blackberry10', dest: t.context.tmp});
+describe('pwa platform', () => {
+	test('pwa - png input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.png', { platform: 'pwa', dest: tempFile });
 
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-90.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-96.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-110.png')));
-	t.true(pathExists.sync(path.join(t.context.tmp, 'icon-144.png')));
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'icon-72x72.png')),
+			pathExists(path.join(tempFile, 'icon-96x96.png')),
+			pathExists(path.join(tempFile, 'icon-128x128.png')),
+			pathExists(path.join(tempFile, 'icon-144x144.png')),
+			pathExists(path.join(tempFile, 'icon-152x152.png')),
+			pathExists(path.join(tempFile, 'icon-192x192.png')),
+			pathExists(path.join(tempFile, 'icon-384x384.png')),
+			pathExists(path.join(tempFile, 'icon-512x512.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
+
+	test('pwa - svg input', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.svg', { platform: 'pwa', dest: tempFile });
+
+		const doesFileExist = await Promise.all([
+			pathExists(path.join(tempFile, 'icon-72x72.png')),
+			pathExists(path.join(tempFile, 'icon-96x96.png')),
+			pathExists(path.join(tempFile, 'icon-128x128.png')),
+			pathExists(path.join(tempFile, 'icon-144x144.png')),
+			pathExists(path.join(tempFile, 'icon-152x152.png')),
+			pathExists(path.join(tempFile, 'icon-192x192.png')),
+			pathExists(path.join(tempFile, 'icon-384x384.png')),
+			pathExists(path.join(tempFile, 'icon-512x512.png')),
+		]);
+		let doesExist = true;
+		doesFileExist.map(item => {
+			if (!item) {
+				doesExist = false;
+			}
+		});
+
+		expect(doesExist).toBe(true);
+	});
 });
 
-test('output size png', async t => {
-	await fn('fixtures/icon.png', {platform: 'ios', dest: t.context.tmp});
+describe('image output size', () => {
+	test('output size: 40 - ios - icon-40.png', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.png', { platform: 'ios', dest: tempFile });
 
-	const image = gm(path.join(t.context.tmp, 'icon-40.png'));
-	const {width, height} = await pify(image.size.bind(image))();
+		const image = gm(path.join(tempFile, 'icon-40.png'));
+		const { width, height } = await pify(image.size.bind(image))();
 
-	t.is(width, 40);
-	t.is(height, 40);
+		expect(width).toBe(40);
+		expect(height).toBe(40);
+	});
+
+	test('output size: 40 - ios - icon-40.svg', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.png', { platform: 'ios', dest: tempFile });
+
+		const image = gm(path.join(tempFile, 'icon-40.png'));
+		const { width, height } = await pify(image.size.bind(image))();
+
+		expect(width).toBe(40);
+		expect(height).toBe(40);
+	});
 });
 
-test('output size svg', async t => {
-	await fn('fixtures/icon.svg', {platform: 'ios', dest: t.context.tmp});
+describe('transparent corners', () => {
+	test('pwa - icon-72x72.png', async () => {
+		const tempFile = tempfile();
+		await fn('fixtures/icon.svg', { platform: 'pwa', dest: tempFile, roundedCorners: true });
 
-	const image = gm(path.join(t.context.tmp, 'icon-40.png'));
-	const {width, height} = await pify(image.size.bind(image))();
+		const { data } = await parsePNG(fs.readFileSync(path.join(tempFile, 'icon-72x72.png')));
 
-	t.is(width, 40);
-	t.is(height, 40);
-});
-
-test('transparent corners', async t => {
-	await fn('fixtures/icon.svg', {platform: 'pwa', dest: t.context.tmp, roundedCorners: true});
-
-	const {data} = await parsePNG(fs.readFileSync(path.join(t.context.tmp, 'icon-72x72.png')));
-
-	// Check the first pixel
-	t.is(data[0], 0); // R
-	t.is(data[1], 0); // G
-	t.is(data[2], 0); // B
-	t.is(data[3], 0); // A
+		// Check the first pixel
+		expect(data[0]).toBe(0); // R
+		expect(data[1]).toBe(0); // G
+		expect(data[2]).toBe(0); // B
+		expect(data[3]).toBe(0); // A
+	});
 });
